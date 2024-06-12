bd46faa27e72e4289d33b46f79ce57838451cabe
Ellen Xu <ellen2imagine@gmail.com>
2024-06-12 13:51:48 -0700

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-// app/page.tsx
 import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="container">
+		<div className="home-container">
 			<h1>The Illustrated Primer</h1>
+			<p>Can you discover this world and its patterns?</p>
 			<Link href="/puzzle1">start</Link>
 		</div>
 	);
